8dd43f8a396da8e8aa440b5ab848e5920ce748b4
Ashok Komaragiri <akomaragiri@ccpace.com>
2018-11-06 16:21:43 -0500

    Reverted back the greeting message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World! This is a new line to test CD"
+    return "Hello World!"
 
 if (__name__ == '__main__'):
     app.run(port=5000, threaded=True, host=('0.0.0.0'))
