b9de28d43f612527f1cf520c5d2f33755ed9245a
Ashok Komaragiri <akomaragiri@ccpace.com>
2018-11-13 12:33:08 -0500

    modified hello world message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
+    return "Hello World! This ia test for Continous Delivery!"
 
 if (__name__ == '__main__'):
     app.run(port=5000, threaded=True, host=('0.0.0.0'))
